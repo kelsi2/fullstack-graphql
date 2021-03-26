2f983512b17cf26289a26fcc4a1f6432eda8e33d
Kelsi Proulx <Kelsiproulx1919@gmail.com>
2021-03-25 17:12:04 -0700

    Setting up ApolloClient, initial query and mutation

--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,19 @@
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
-import gql from 'graphql-tag'
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import gql from "graphql-tag";
 
 /**
  * Create a new apollo client and export as default
  */
+
+// This is where our API lives
+const link = new HttpLink({ uri: "http://localhost:4000/" });
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link,
+  cache,
+});
+
+export default client;
--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,21 +1,57 @@
-import React, {useState} from 'react'
-import gql from 'graphql-tag'
-import { useQuery, useMutation } from '@apollo/react-hooks'
-import PetsList from '../components/PetsList'
-import NewPetModal from '../components/NewPetModal'
-import Loader from '../components/Loader'
+import React, { useState } from "react";
+import gql from "graphql-tag";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import PetsList from "../components/PetsList";
+import NewPetModal from "../components/NewPetModal";
+import Loader from "../components/Loader";
+import { AddTypenameToAbstract } from "graphql-tools";
 
-
-export default function Pets () {
-  const [modal, setModal] = useState(false)
-
-
-  const onSubmit = input => {
-    setModal(false)
+const ALL_PETS = gql`
+  query AllPets {
+    pets {
+      name
+      id
+      type
+      img
+    }
   }
-  
+`;
+
+const NEW_PET = gql`
+  mutation createPet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      id
+      name
+      type
+      img
+    }
+  }
+`;
+
+export default function Pets() {
+  const [modal, setModal] = useState(false);
+  const { data, loading, error } = useQuery(ALL_PETS);
+  const [createPet, newPet] = useMutation(NEW_PET);
+
+  const onSubmit = (input) => {
+    setModal(false);
+    createPet({
+      variables: {
+        newPet: input,
+      },
+    });
+  };
+
+  if (loading || newPet.loading) {
+    return <Loader />;
+  }
+
+  if (error || newPet.error) {
+    return <p>error</p>;
+  }
+
   if (modal) {
-    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
+    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />;
   }
 
   return (
@@ -32,8 +68,8 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets} />
       </section>
     </div>
-  )
+  );
 }
